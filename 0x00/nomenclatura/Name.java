672ac0cc576473f57f75aca948d52c55cfc25070
Anderson Pazin <a.pazin@gmail.com>
2025-02-08 12:24:23 -0300

    Add files via upload

---
+++ 0x00/nomenclatura/Name.java
@@ -0,0 +1,40 @@
+import java.util.Date;
+import java.util.List;
+import java.net.URL;
+
+public class Name {
+
+    public Date modifiedDate;
+    public List<Testcase> findAllTestcasesByUser(User user){ return null;};
+
+    public List<Account> accountList;
+
+    public void deleteUser(User user){};
+
+    public URL url;
+
+    public class TableRepresenter{};
+    public void saveCar(){};
+
+    public void saveUser(){};
+    public void createAccount(){};
+    public void generateContract(){};
+
+    public static final int DAYS_IN_YEAR = 360;
+    public static final int HOURS_IN_WEEK = 168;
+
+    // Classe interna User (exemplo)
+    public class User {
+        // ... atributos e métodos da classe User
+    }
+
+    // Classe interna Testcase (exemplo)
+    public class Testcase {
+        // ... atributos e métodos da classe Testcase
+    }
+
+    // Classe interna Account (exemplo)
+    public class Account {
+        // ... atributos e métodos da classe Account
+    }
+}
