8d2783cc92c0a4660bc4c756502b8a0abe496d9d
Anderson Pazin <a.pazin@gmail.com>
2025-02-11 15:35:25 -0300

    Add files via upload

--- 0x00/nomenclatura/Name.java
+++ 0x00/nomenclatura/Name.java
@@ -17,18 +17,13 @@ public class Name {
     public URL url;
 
     // Classe possui nomes próprios, funções devem ter verbos
-    //A
-    public class TableRepresent {};
-    public void saveCar(){};
-//    //B
-//    public class TableRepresenter{};
-//    public void car(){};
+    //B
+    public class TableRepresenter{};
+    public void car(){};
 
     // Uma palavra por conceito
-    //A
-    public saveUser(){}; saveAccount(){}; saveContract(){};
-//    //B
-//    public saveUser(){}; createAccount(){}; generateContract(){};
+    //B
+    public saveUser(){}; createAccount(){}; generateContract(){};
 
 
     //Use constants, enums instead of magic numbers, abbreviations
@@ -38,3 +33,4 @@ public class Name {
     //B
     public final int DAYS_IN_YEAR = 360;
     public final int HOURS_IN_WEEK = 168;
+    }
